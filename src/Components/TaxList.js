53010f706309cd5e374d776d1bbabe031036e2ee
DESKTOP-BDM4U1J\eund <eund@ubeeslab.com>
2022-06-30 08:30:43 +0900

    maybe last

--- src/Components/TaxList.js
+++ src/Components/TaxList.js
@@ -5,13 +5,14 @@ const TaxList = (props) => {
     return (
         <>
             <ListWapper>
-                <p><List25Span>취득세</List25Span><List40Span>{tax.tax1 && tax.tax1}</List40Span><List10Span>원</List10Span><ShowDetailSpan>자세히보기</ShowDetailSpan></p>
-                <p><List25Span>재산세</List25Span><List40Span>{tax.tax2}</List40Span><List10Span>원</List10Span><ShowDetailSpan>자세히보기</ShowDetailSpan></p>
-                <p><List25Span>종합소득세</List25Span><List40Span>{tax.tax3}</List40Span><List10Span>원</List10Span><ShowDetailSpan>자세히보기</ShowDetailSpan></p>
-                <p><List25Span>건강보험료</List25Span><List40Span>{tax.tax4}</List40Span><List10Span>원</List10Span><ShowDetailSpan>자세히보기</ShowDetailSpan></p>
-                <p><List25Span>부가가치세</List25Span><List40Span>{tax.tax5}</List40Span><List10Span>원</List10Span><ShowDetailSpan>자세히보기</ShowDetailSpan></p>
-                <p><List25Span>대출이자</List25Span><List40Span>{"-"}</List40Span><List10Span>원</List10Span><ShowDetailSpan>자세히보기</ShowDetailSpan></p>
-                <p><List25Span>공실비용</List25Span><List40Span>{"-"}</List40Span><List10Span>원</List10Span><ShowDetailSpan>자세히보기</ShowDetailSpan></p>
+                <p><List25Span>취득세</List25Span><List40Span>{tax.tax1 && tax.tax1}</List40Span><List10Span>원</List10Span><ShowDetailSpan></ShowDetailSpan></p>
+                <p><List25Span>재산세</List25Span><List40Span>{tax.tax2}</List40Span><List10Span>원</List10Span><ShowDetailSpan></ShowDetailSpan></p>
+                <p><List25Span>종합소득세</List25Span><List40Span>{tax.tax3}</List40Span><List10Span>원</List10Span><ShowDetailSpan></ShowDetailSpan></p>
+                <p><List25Span>건강보험료</List25Span><List40Span>{Math.round(tax.tax4 * 100)/100}</List40Span><List10Span>원</List10Span><ShowDetailSpan></ShowDetailSpan></p>
+                <p><List25Span>부가가치세</List25Span><List40Span>{tax.tax5}</List40Span><List10Span>원</List10Span><ShowDetailSpan></ShowDetailSpan></p>
+                <p><List25Span>공실비용</List25Span><List40Span>{Math.round(tax.loss1 * 100)/100}</List40Span><List10Span>원</List10Span><ShowDetailSpan></ShowDetailSpan></p>
+                <p><List25Span>대출이자</List25Span><List40Span>{Math.round(tax.loss2 * 100)/100}</List40Span><List10Span>원</List10Span><ShowDetailSpan></ShowDetailSpan></p>
+                <p><List25Span>투자금회수기간</List25Span><List40Span>{"-"}</List40Span><List10Span>년</List10Span><ShowDetailSpan>{tax.yearyearyear}</ShowDetailSpan></p>
             </ListWapper>
         </>
     )
--- src/Routes/Tax.js
+++ src/Routes/Tax.js
@@ -7,7 +7,7 @@ import { useParams } from "react-router-dom";
 const Tax = () => {
     const params = useParams();
 
-    const [tax, setTax] = useState({tax1:0, tax2:0, tax3:0, tax4:0, tax5:0});
+    const [tax, setTax] = useState({tax1:0, tax2:0, tax3:0, tax4:0, tax5:0, loss1:0, loss2:0, yearyearyear:0});
 
     const searchOnClick = (price, investmentPrice) => {
         console.log('this is working');
@@ -31,11 +31,11 @@ const Tax = () => {
         if (priceInt <= 60000000) {
             tax2 = priceInt * 0.001;
         } else if (priceInt <= 150000000) {
-            tax2 = 60000 + 0.015 * (priceInt - 60000000);
+            tax2 = 60000 + 0.0015 * (priceInt - 60000000);
         } else if (priceInt <= 300000000) {
-            tax2 = 195000 + 0.025 * (priceInt - 150000000);
+            tax2 = 195000 + 0.0025 * (priceInt - 150000000);
         } else {
-            tax2 = 490000 + 0.04 * (priceInt - 300000000);
+            tax2 = 490000 + 0.004 * (priceInt - 300000000);
         }
 
         const income = priceInt * 0.04;
@@ -58,11 +58,63 @@ const Tax = () => {
             tax3 = income * 0.45;
         }
 
-        tax4 = 40000000 * 0.0368 + (income - 34000000) * 0.0667;
+        tax4 = 0;
+        if (income > 34000000) {
+            tax4 = 40000000 * 0.0368 + (income - 34000000) * 0.0667;
+        }
 
         tax5 = income * 0.1;
 
-        setTax((prevState) => ({ ...prevState, tax1: tax1, tax2: tax2, tax3: tax3, tax4: tax4, tax5: tax5 }));
+        const loss1 = income * 0.027089;
+        console.log('아 이건 또 뭐야', loss1);
+        let loss2 = investmentPrice * 0.01;
+        
+        const k = price / (income - tax1 - tax2 - tax3 - tax4 - tax5 - loss1 - loss2);
+
+        let interestRate = 1.3421 + 3.0;
+        const loss2Org = investmentPrice * interestRate * 0.01;
+        console.log("근데 왜 음수가 나와", loss2Org);
+
+        let yearyearyear = 0;
+        const investmentPriceOrg = investmentPrice;
+
+        // console.log(income, loss1, investmentPrice * interestRate * 0.01)
+        // investmentPrice = investmentPrice - (income - tax1 - tax2 - tax3 - tax4 - tax5 - loss1 - investmentPrice * interestRate * 0.01);
+        // interestRate = 1.3421 + 3.0 + ((2.6818 + 3.0) - interestRate) * 0.25;
+        // console.log(interestRate, investmentPrice, investmentPrice * interestRate * 0.01);
+
+        console.log('아', loss2);
+
+        while (investmentPrice > 0 ){
+            if ((income - tax1 - tax2 - tax3 - tax4 - tax5 - loss1 - loss2) < 1 ) {
+                console.log("혹쉬 이거 걸리나");
+                if (yearyearyear < 2) {
+                    alert("투자금을 낮춰주세요. 대출금리는 최근 금리를 반영하여 높게 측정됩니다.")
+                }
+                break;
+            }
+            console.log("무슨 차이가 있지?", investmentPrice, income, tax1 + tax2 + tax3 + tax4 + tax5, loss1, loss2);
+            investmentPrice = investmentPrice - (income - tax1 - tax2 - tax3 - tax4 - tax5 - loss1 - loss2);
+            // investmentPrice = investmentPrice - (income - tax1 - tax2 - tax3 - tax4 - tax5 - loss1 - investmentPrice * interestRate * 0.01);
+            interestRate = 1.3421 + 3.0 + ((2.6818 + 3.0) - interestRate) * 0.25;
+            loss2 = investmentPrice * interestRate * 0.01;
+            if (loss2 < 1) {
+                console.log("혹쉬 이거 걸리나");
+                if (yearyearyear < 2) {
+                    alert("투자금을 낮춰주세요. 대출금리는 최근 금리를 반영하여 높게 측정됩니다.")
+                }
+                break;
+            }
+            // console.log(loss2);
+            // console.log(investmentPrice, income - tax1 - tax2 - tax3 - tax4 - tax5 - loss1 - loss2);
+            yearyearyear = yearyearyear + 1;
+            // console.log(interestRate);
+            // tic = tic + 1;
+        }
+
+        console.log(yearyearyear);
+
+        setTax((prevState) => ({ ...prevState, tax1: tax1, tax2: tax2, tax3: tax3, tax4: tax4, tax5: tax5, loss1: loss1, loss2: loss2Org, yearyearyear: yearyearyear }));
     } 
     
     return (
